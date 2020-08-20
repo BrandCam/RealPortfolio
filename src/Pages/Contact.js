a03088efc5d5fd1413b07ba0d6834fe5faded705
BrandCam <ufc.campbell@gmail.com>
2020-08-20 05:13:03 -0500

    contact page functional

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -3,6 +3,20 @@ import NavBar from "../Components/NavBar/NavBar";
 import Video from "../Components/Video/Video";
 import PageWrap from "./Page.style";
 import LargeCard from "../Components/UI/LargeCard";
+import styled from "styled-components";
+import { Formik, Field, Form } from "formik";
+import emailjs from "emailjs-com";
+
+const InfoWrap = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  p {
+    text-align: center;
+    margin: 0 1rem;
+    font-size: 1.4rem;
+  }
+`;
 const Contact = ({ vp, page }) => {
   //this is stupid and so am I
   useEffect(() => {
@@ -14,8 +28,48 @@ const Contact = ({ vp, page }) => {
   return (
     <PageWrap>
       <NavBar page={page} />
-      <Video />
-      <LargeCard></LargeCard>
+      <LargeCard size="small">
+        <InfoWrap>
+          <p>Feel free to shoot me an email. </p>
+          <p>
+            For a more concise reply please include a brief message detailing
+            what I can help you with.
+          </p>
+        </InfoWrap>
+      </LargeCard>
+      <Formik
+        initialValues={{
+          name: "",
+          email: "",
+          subject: "",
+          message: "",
+        }}
+        onSubmit={(values) => {
+          emailjs
+            .send(
+              "gmail",
+              "portfolio_reply",
+              values,
+              "user_OiYECJTwy5GRU6wqSW2LR"
+            )
+            .then(
+              (res) => {
+                console.log("SUCCESS", res.status, res.text);
+              },
+              (err) => {
+                console.log("FAILED", err);
+              }
+            );
+        }}
+      >
+        <Form>
+          <Field name="name" type="name" />
+          <Field name="email" type="email" />
+          <Field name="subject" type="text" />
+          <Field name="message" type="text" />
+          <button type="submit">SEND</button>
+        </Form>
+      </Formik>
     </PageWrap>
   );
 };
