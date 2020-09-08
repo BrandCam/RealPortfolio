1e3bd289eac10fa457cc993ecbd58d6405e5c4cd
BrandCam <ufc.campbell@gmail.com>
2020-09-07 21:26:36 -0500

    about update

--- src/Components/AboutChunk/AboutOne.js
+++ src/Components/AboutChunk/AboutOne.js
@@ -22,8 +22,8 @@ const AboutOne = (props) => {
         tutorial do not equate to knowing a language. Writing a web server in C
         was probably the hardest bit of programming I have ever done, and that's
         with using a tutorial. I may not have gained much In terms of C writing
-        ability, but I did take away one thing, the power of direct memory
-        management. That is why my next language of choice is going to be RUST.
+        ability, but I did take away one thing, the power of a very fast
+        language. That is why my next language of choice is going to be RUST.
         RUST should give me enough hand holding with their excellent compiler
         and error messages ,and great support for WebAssembly to boot.
       </p>
